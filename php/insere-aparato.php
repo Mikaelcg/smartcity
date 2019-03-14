0a4c3e6e258b5d715629d46510f1785b6ae7ef90
johnnydueck <jmd1407@gmail.com>
2019-03-14 10:52:32 -0300

    Tela de cadastro de aparatos

---
+++ php/insere-aparato.php
@@ -0,0 +1,18 @@
+<?php
+	
+	$aparatoDomestico  =       $_POST["aparato"];
+    $residenciaAparato =       $_POST["residenciaAparato"]; 
+
+	$endereco = "localhost:8889";
+	$usuario_banco = "root";
+	$senha_banco = "root";
+	$nome_banco = "banco_bsi";
+
+	$conexao = mysqli_connect($endereco, $usuario_banco, $senha_banco, $nome_banco);
+
+    echo(" INSERT INTO habitante(nome, sobrenome, nascimento, sexo) VALUES('$nome','$sobrenome','$dataNasc', '$sexo') ");
+     
+	mysqli_query($conexao, " INSERT INTO aparatoDomestico(aparato, residenciaAparato) VALUES('$aparatoDomestico','$residenciaAparato') ");
+
+
+?>
