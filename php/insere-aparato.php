e26deecad7f1df167cf03914a84d1177832553a3
Lucas <antonio.conceicao@pucpr.edu.br>
2019-03-14 20:41:55 -0300

    Arrumado o Cadastro de Aparatos Domésticos

--- php/insere-aparato.php
+++ php/insere-aparato.php
@@ -3,14 +3,14 @@
 	$aparatoDomestico  =       $_POST["aparato"];
     $residenciaAparato =       $_POST["residenciaAparato"]; 
 
-	$endereco = "localhost:8889";
+	$endereco = "localhost";
 	$usuario_banco = "root";
-	$senha_banco = "root";
+	$senha_banco = "";
 	$nome_banco = "banco_bsi";
 
 	$conexao = mysqli_connect($endereco, $usuario_banco, $senha_banco, $nome_banco);
 
-    echo(" INSERT INTO habitante(nome, sobrenome, nascimento, sexo) VALUES('$nome','$sobrenome','$dataNasc', '$sexo') ");
+    echo(" INSERT INTO aparatoDomestico(aparato, residenciaAparato) VALUES('$aparatoDomestico','$residenciaAparato') ");
      
 	mysqli_query($conexao, " INSERT INTO aparatoDomestico(aparato, residenciaAparato) VALUES('$aparatoDomestico','$residenciaAparato') ");
 
