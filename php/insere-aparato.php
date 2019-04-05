43b1689718aa1e3b75d4ee29926dc41df9dddb27
Mikael <mikagallucci@hotmail.com>
2019-04-04 21:53:37 -0300

    Ajustes

--- php/insere-aparato.php
+++ php/insere-aparato.php
@@ -3,11 +3,7 @@
 	$aparatoDomestico  =       $_POST["aparato"];
     $residenciaAparato =       $_POST["residenciaAparato"]; 
 
-<<<<<<< HEAD
 	$endereco = "localhost:8889";
-=======
-	$endereco = "localhost";
->>>>>>> ec584bd671d370e3b48e25c30007ea9029f577c9
 	$usuario_banco = "root";
 	$senha_banco = "";
 	$nome_banco = "banco_bsi";
@@ -17,6 +13,5 @@
     echo(" INSERT INTO aparatodomestico(aparato, id_residencia) VALUES('$aparatoDomestico','$residenciaAparato') ");
      
 	mysqli_query($conexao, " INSERT INTO aparatodomestico(aparato, id_residencia) VALUES('$aparatoDomestico','$residenciaAparato') ");
-
 
 ?>
