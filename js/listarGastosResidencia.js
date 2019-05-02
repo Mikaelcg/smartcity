91a9c86221855ea3b3f9e01b0ec5b72bb376ceb0
Julio Pereira <juliocp.next@gmail.com>
2019-05-02 20:55:46 -0300

    Total de energia gasta por casa

--- js/listarGastosResidencia.js
+++ js/listarGastosResidencia.js
@@ -17,9 +17,8 @@ function listaGastos(){
 
             for(var i = 0; i < array["gastos"].length; i++){
                 conteudo_gastos += "<tr>";
-                conteudo_gastos += "<td>" + array["gastos"][i]["id"] + "</td>";
-                conteudo_gastos += "<td>" + array["gastos"][i]["endereco"] + ", " + array["gastos"][i]["numero"] + "</td>";
-                conteudo_gastos += "<td>" + array["gastos"][i]["consumo"] + "</td>";
+                conteudo_gastos += "<td>" + array["gastos"][i]["id_residencia"] + "</td>";
+                conteudo_gastos += "<td>" + array["gastos"][i]["total"] + "</td>";
                 conteudo_gastos += "</tr>";
             }
 
