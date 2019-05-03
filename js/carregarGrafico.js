f0f07b178c80c7cc5d28241eaf65c43913954e80
Jonathan <jmd1407@gmail.com>
2019-05-02 22:37:02 -0300

    Grafico sem funcinar

---
+++ js/carregarGrafico.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+	
+	carregarGrafico();
+
+});
+
+
+function carregarGrafico(){
+
+
+	var ctx = document.getElementById('myChart').getContext('2d');
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            
+            datasets: [{
+                label: 'Consumo (dado/mês)',
+				data: [],
+          		
+                borderColor: [
+                    'rgba(0, 255, 299, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+    fLocalComunicaServidorGrafico(myChart);
+}
+
+
+function fLocalComunicaServidorGrafico(myChart){
+
+
+	$.ajax({
+		type: "POST",
+		dataType: "json",
+		url: "../php/dadosGrafico.php",
+		success:function(array){
+            console.log(array[2]);
+            for(x in array){
+                myChart.data.datasets[0].data.push(array[x].consumo_eletrico);
+                myChart.data.datasets[0].data.push(array[x].id_residencia);
+                myChart.data.labels.push(array[x].residencia);
+                
+                
+            }
+		}
+	});
+	
+}
---
+++ php/dadosGrafico.php
@@ -0,0 +1,33 @@
+<?php
+    
+    $id_residencia = $_POST["id_residencia"];
+    
+    $endereco = "localhost:8889";
+	$usuario_banco = "root";
+	$senha_banco = "root";
+	$nome_banco = "banco_bsi";
+
+    $conexao = mysqli_connect($endereco, $usuario_banco, $senha_banco, $nome_banco);
+    $dados = mysqli_query($conexao, "SELECT id_residencia, tempo_consumo FROM consumo_eletrico" );
+
+    $cont = 0;
+
+    
+
+    while($registro = mysqli_fetch_assoc($dados)){
+        $cont++;
+        $retorno[$cont]["residencia"] = $registro["id_residencia"];
+        $retorno[$cont]["consumo_eletrico"] = $registro["tempo_consumo"]; 
+        
+    }
+
+    if($cont > 0){
+        $retorno["status"] = "s";
+
+        echo json_encode($retorno);
+    }else{
+        $retorno["status"] = "n";
+
+        echo("erro");
+    }
+?>
