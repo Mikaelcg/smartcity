3096c274ee5d53e9e27f5ebb9c68f66023d964e5
mikael <mikagallucci@hotmail.com>
2019-05-23 21:05:07 -0300

    Ajuste query grafico

--- dist/php/pages/graficos/grafico-gastos.php
+++ dist/php/pages/graficos/grafico-gastos.php
@@ -5,16 +5,18 @@
 
     $conexao = mysqli_connect($endereco, $usuario_banco, $senha_banco, $nome_banco);
     
-    $dados = mysqli_query($conexao, "SELECT c.id , c.id_residencia, SUBSTR(c.data_consumo,4, 2) as mes_consumo, sum(c.tempo_consumo * a.potencia) as total FROM consumo_eletrico c join aparatodomestico a 
+    $dados = mysqli_query($conexao, "SELECT c.id_residencia, SUBSTR(c.data_consumo,4, 2) as mes_consumo, sum(c.tempo_consumo * a.potencia) as total FROM consumo_eletrico c join aparatodomestico a 
         WHERE c.id_aparato = a.id 
         and c.id_residencia = $casa
         group by mes_consumo;");
     
     $cont = 0;
 
+
     $json_array = array();
 
     while($registro = mysqli_fetch_assoc($dados)){
+        // echo($registro);
         $json_array[] = $registro;
     }
 
